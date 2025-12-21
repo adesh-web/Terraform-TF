03d8c233bf4ebad2cabbaeb4995c54f15f608bf4
adesh-web <adeshashokashtikar@gmail.com>
2025-12-21 21:28:26 +0530

    s3 add

---
+++ Terraform-TF/SECURE-BACKUP/tf-backend-secure-file.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "myadityakadu"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+    }
+}
+
+
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami = "ami-0fa91bc90632c73c9"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "My_instance_1"
+        env  = "dev"
+    }
+  
+}
